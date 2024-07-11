9d5e71a5890bd5d9bb85f3fb52bae225c578c505
AhmedAtef202 <ahmedatefhussien10@gmail.com>
2024-07-11 07:00:47 +0300

    add bordor raidus to button sheet

--- lib/views/notes_views.dart
+++ lib/views/notes_views.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:untitled/views/widgets/add_note_button_sheet.dart';
 import 'package:untitled/views/widgets/notes_view_body.dart';
 
@@ -11,6 +12,9 @@ class NotesViews extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           showModalBottomSheet(
+            shape:  RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16),
+            ),
               context: context,
               builder: (context) {
                 return const AddNoteButtomSheet();
