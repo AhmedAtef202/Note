0a711f0e5883af6c28636481c4253a203c4e8e14
AhmedAtef202 <ahmedatefhussien10@gmail.com>
2024-07-11 03:54:33 +0300

    create NotesApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp();
+  runApp(const NotesApp());
+}
+
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
 }
