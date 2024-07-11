bcf215ea817defa1c32dbef1c1226364c6dfdb68
AhmedAtef202 <ahmedatefhussien10@gmail.com>
2024-07-11 07:19:47 +0300

    create custom text field

--- lib/views/notes_views.dart
+++ lib/views/notes_views.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:untitled/views/widgets/add_note_button_sheet.dart';
 import 'package:untitled/views/widgets/notes_view_body.dart';
 
